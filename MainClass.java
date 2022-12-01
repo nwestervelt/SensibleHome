0df6864ab7ee8e471dcf41fd2136856eb069a295
Noah Westervelt <nwestervelt410@gmail.com>
2022-12-01 00:19:59 -0600

    Create initial UI and add sockets.
    Create an initial, simple, UserInterface implementation to facilitate
    testing of other components.
    
    Add sockets to MainClass and UserInterface, MainClass being a
    multi-threaded server to UserInterface instances. UserInterface can send
    text to MainClass using buttons and disconnect, also using buttons.

--- MainClass.java
+++ MainClass.java
@@ -1,15 +1,83 @@
 //Class file for the main class, entry point of the system for the server
 
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
 public class MainClass
 {
     public MainClass()
     {
+        ServerSocket servSock;
+
+        try
+        {
+            ClientThread ct;
+
+            //create a server socket on port 5000
+            servSock = new ServerSocket(5000);
+
+            while(true)
+            {
+                //program blocks here until a client connects
+                ct = new ClientThread(servSock.accept());
+                ct.start();
+            }
+        }
+        catch(IOException ioe)
+        {
+            System.out.println(ioe);
+            System.exit(1);
+        }
     }
     //thread for handling requests from clients (UserInterface instances)
     private class ClientThread extends Thread
     {
+        private Scanner in;
+
+        public ClientThread(Socket sock)
+        {
+
+            try
+            {
+                in = new Scanner(sock.getInputStream());
+            }
+            catch(IOException ioe)
+            {
+                System.out.println(ioe);
+                System.exit(1);
+            }
+        }
+        //this method runs whenever a client connects,
+        //executed by calling start() method of this class 
         public void run()
         {
+            try
+            {
+                //loop while the client is connected
+                while(true)
+                {
+                    if(in.hasNext())
+                    {
+                        String command = in.nextLine();
+
+                        //process commands sent over network here
+
+                        //print out the command for debugging purposes
+                        System.out.println(command);
+
+                        //exit loop if client disconnects
+                        if(command.equals("quit"))
+                            break;
+                    }
+                    //sleep to keep from using CPU cycles unnecessarily while waiting
+                    sleep(50);
+                }
+            }
+            catch(InterruptedException ie)
+            {
+                System.out.println(ie);
+            }
         }
     }
     public static void main(String[] args)
--- UserInterface.java
+++ UserInterface.java
@@ -8,14 +8,157 @@ import javax.swing.*;
 
 public class UserInterface extends JFrame
 {
+    private PrintWriter out;
+    private Socket sock;
+    private JButton connectButton, testButton, quitButton;
+
     public UserInterface()
     {
+        //create ActionHandler to be used in buttons
+        ActionHandler ah = new ActionHandler();
+
+        //create and add the mainPanel
+        JPanel mainPanel = new JPanel();
+        mainPanel.setPreferredSize(new Dimension(500, 500));
+        add(mainPanel);
+
+        //create connectButton and add event handling
+        connectButton = new JButton("Connect");
+        connectButton.addActionListener(ah);
+        mainPanel.add(connectButton);
+
+        //create testButton and add event handling
+        testButton = new JButton("Test");
+        testButton.addActionListener(ah);
+        testButton.setEnabled(false);
+        mainPanel.add(testButton);
+
+        //create quitButton and add event handling
+        quitButton = new JButton("Quit");
+        quitButton.addActionListener(ah);
+        quitButton.setEnabled(false);
+        mainPanel.add(quitButton);
+
+        //set appearance and behavior of the window
+        pack();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
     }
     //class for handling user interface events (buttons clicked, etc..)
     private class ActionHandler implements ActionListener
     {
         public void actionPerformed(ActionEvent e)
         {
+            if(e.getSource() == connectButton)
+            {
+                ConnectDialog conD = new ConnectDialog();
+
+                //blocks here while dialog box is visible
+                conD.setVisible(true);
+
+                //if submitted button in dialog box was clicked, connect
+                if(conD.submitted)
+                {
+                    try
+                    {
+                        //attempt to connect
+                        sock = new Socket(conD.ipField.getText(), Integer.parseInt(conD.portField.getText()));
+
+                        //create PrintWriter for writing to socket
+                        out = new PrintWriter(sock.getOutputStream(), true);
+
+                        //update enabled status of buttons
+                        connectButton.setEnabled(false);
+                        testButton.setEnabled(true);
+                        quitButton.setEnabled(true);
+                    }
+                    catch(Exception ex)
+                    {
+                        System.out.println(ex);
+                    }
+                }
+            }
+            else if(e.getSource() == testButton)
+            {
+                //sends "test" text over socket
+                out.println("test");
+            }
+            else if(e.getSource() == quitButton)
+            {
+                //disconnects by sending "quit" over the socket
+                out.println("quit");
+
+                //set out to null and disconnect
+                out = null;
+                try
+                {
+                    sock.close();
+                }
+                catch(IOException ioe)
+                {
+                    System.out.println(ioe);
+                }
+
+                //update enabled status of buttons
+                connectButton.setEnabled(true);
+                testButton.setEnabled(false);
+                quitButton.setEnabled(false);
+            }
+        }
+    }
+    private class ConnectDialog extends JDialog
+    {
+        private boolean submitted;
+        private JTextField ipField, portField;
+
+        public ConnectDialog()
+        {
+            //make modal (block parent's execution while visible)
+            super(UserInterface.this, true);
+
+            //initialize submitted to false
+            submitted = false;
+
+            //create and change layout manager of panel
+            JPanel dialogPanel = new JPanel();
+            dialogPanel.setPreferredSize(new Dimension(400, 200));
+            dialogPanel.setLayout(new BoxLayout(dialogPanel, BoxLayout.Y_AXIS));
+            add(dialogPanel);
+
+            //create panel, label, and text field for ip address
+            JPanel ipPanel = new JPanel();
+            dialogPanel.add(ipPanel);
+
+            JLabel ipLabel = new JLabel("IP Address: ");
+            ipPanel.add(ipLabel);
+
+            ipField = new JTextField(20);
+            ipPanel.add(ipField);
+
+            //create panel, label, and text field for port number
+            JPanel portPanel = new JPanel();
+            dialogPanel.add(portPanel);
+
+            JLabel portLabel = new JLabel("Port: ");
+            portPanel.add(portLabel);
+
+            portField = new JTextField(20);
+            portPanel.add(portField);
+
+            //create submit button and add custom ActionListener to it
+            JButton dialogSubmitButton = new JButton("Submit");
+            dialogSubmitButton.addActionListener(new ActionListener()
+            {
+                public void actionPerformed(ActionEvent e)
+                {
+                    submitted = true;
+                    setVisible(false);
+                }
+            });
+            dialogPanel.add(dialogSubmitButton);
+
+            //set appearance
+            pack();
         }
     }
     public static void main(String[] args)
