c567011e23fd9a15e5bffb986432b7aef12b7b28
StitchsFriend <60453878+StitchsFriend@users.noreply.github.com>
2022-12-05 18:16:31 -0600

    Update Bed.java
    new update

--- Bed.java
+++ Bed.java
@@ -2,15 +2,24 @@
 
 public class Bed extends Device
 {
+    private int temp;
     public Bed(int deviceID)
     {
         super(deviceID);
     }
     public int getTemp()
     {
-        return 50;
+        return temp;
     }
     public void setTemp(int temp)
     {
+        if(0<= temp && temp<= 100)
+            temp= temp;// set temperature
+            else
+        {
+            System.out.println(
+                    "New temp out of range.  Setting to default of 50F" );
+            temp = 50;                 // Set to default if t out of range
+        }
     }
 }
